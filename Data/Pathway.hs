90428eec4c567ea9bbeda19a99d8ba0365d67909
Murat Kasimov <iokasimov.m@gmail.com>
2018-09-24 13:57:54 +0300

    Define only four allowed opearations on path concatenation

--- Data/Pathway.hs
+++ Data/Pathway.hs
@@ -1,6 +1,6 @@
 module Data.Pathway where
 
-import "base" Data.Function (($))
+import "base" Data.Function ((.), ($))
 import "base" Data.Maybe (Maybe (Just, Nothing))
 import "base" Data.String (String)
 import "free" Control.Comonad.Cofree (Cofree ((:<)))
@@ -19,3 +19,19 @@ data Path (reference :: Reference) (points :: Points) = Path (Stack String)
 -- | Immerse some string into a path component
 part :: String -> Path referece points
 part x = Path $ x :< Nothing
+
+(<^@>) :: Path Relative Directory -> Path Relative Directory -> Path Relative Directory
+Path (x :< Nothing) <^@> Path that = Path $ x :< Just that
+Path (x :< Just this) <^@> Path that = part x <^@> (Path this <^@> Path that)
+
+(<^#>) :: Path Relative Directory -> Path Relative File -> Path Relative File
+Path (x :< Nothing) <^#> Path that = Path $ x :< Just that
+Path (x :< Just this) <^#> Path that = part x <^#> (Path this <^#> Path that)
+
+(</@>) :: Path Absolute Directory -> Path Relative Directory -> Path Absolute Directory
+Path absolute </@> Path (x :< Nothing) = Path . (:<) x . Just $ absolute
+Path absolute </@> Path (x :< Just xs) = (Path . (:<) x . Just $ absolute) </@> Path xs
+
+(</#>) :: Path Absolute Directory -> Path Relative File -> Path Absolute File
+Path absolute </#> Path (x :< Nothing) = Path . (:<) x . Just $ absolute
+Path absolute </#> Path (x :< Just xs) = (Path . (:<) x . Just $ absolute) </#> Path xs
