b5ee1e34d1480efe4c95de5ad033f61781c6de7f
Murat Kasimov <iokasimov.m@gmail.com>
2018-09-30 16:24:40 +0300

    Create part and main combinators functions

--- System/Monopati/Posix.hs
+++ System/Monopati/Posix.hs
@@ -1,9 +1,10 @@
 module System.Monopati.Posix where
 
+import "base" Data.Function ((.), ($))
 import "base" Data.Kind (Type)
-import "base" Data.Maybe (Maybe)
+import "base" Data.Maybe (Maybe (Just, Nothing))
 import "base" Data.String (String)
-import "free" Control.Comonad.Cofree (Cofree)
+import "free" Control.Comonad.Cofree (Cofree ((:<)))
 
 data Points = Directory | File -- What the path points to?
 data Origin = Root | Home | Vague -- What is the beginning of the path?
@@ -25,3 +26,18 @@ type family Homeward (path :: Type) (to :: Type) (points :: Points) :: Type wher
 
 type family Relative (path :: Type) (to :: Type) (points :: Points) :: Type where
 	Relative Path To points = Outline Vague points
+
+part :: String -> Outline origin points
+part x = Outline $ x :< Nothing
+
+(<^>) :: Relative Path To Directory -> Relative Path To points -> Relative Path To points
+Outline (x :< Nothing) <^> Outline that = Outline $ x :< Just that
+Outline (x :< Just this) <^> Outline that = part x <^> (Outline this <^> Outline that)
+
+(</>) :: Absolute Path To Directory -> Relative Path To points -> Absolute Path To points
+Outline absolute </> Outline (x :< Nothing) = Outline . (:<) x . Just $ absolute
+Outline absolute </> Outline (x :< Just xs) = (Outline . (:<) x . Just $ absolute) </> Outline xs
+
+(<~/>) :: Absolute Path To points -> Homeward Path To points -> Absolute Path To points
+Outline absolute <~/> Outline (x :< Nothing) = Outline . (:<) x . Just $ absolute
+Outline absolute <~/> Outline (x :< Just xs) = (Outline . (:<) x . Just $ absolute) <~/> Outline xs
