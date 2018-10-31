3c9d1d7b52a6dd8eb848e6efcb5dfc2e2e5f96a0
Murat Kasimov <iokasimov.m@gmail.com>
2018-10-31 22:16:18 +0300

    Add tests for checking Show and Read instances for absolute and relative paths

--- Test/Monopati.hs
+++ Test/Monopati.hs
@@ -4,11 +4,11 @@ import "base" Data.Function (($))
 import "base" System.IO (BufferMode(..), hSetBuffering, stdout, stderr)
 import "hedgehog" Hedgehog (Group (..), checkParallel)
 
-import Test.Monopati.Posix (concatenate_absolute_and_relative, show_then_read_absolute)
+import Test.Monopati.Posix (show_then_read_absolute, show_then_read_relative)
 
 main = do
 	hSetBuffering stdout LineBuffering
 	hSetBuffering stderr LineBuffering
 	checkParallel $ Group "Simple Posix tests" [
-		("concatenate_absolute_and_relative", concatenate_absolute_and_relative),
-		("show_then_read_absolute", show_then_read_absolute)]
+		("Show then read absolute", show_then_read_absolute),
+		("Show then read relative", show_then_read_relative)]
--- Test/Monopati/Posix.hs
+++ Test/Monopati/Posix.hs
@@ -1,35 +1,45 @@
-module Test.Monopati.Posix (concatenate_absolute_and_relative, show_then_read_absolute) where
+module Test.Monopati.Posix (show_then_read_absolute, show_then_read_relative) where
 
-import "base" Control.Applicative ((<*>))
+import "base" Control.Applicative (pure, (<*>))
 import "base" Control.Monad ((>>=))
 import "base" Data.Bool (Bool (True))
-import "base" Data.Function ((.), ($))
+import "base" Data.Foldable (foldr)
+import "base" Data.Function (flip, (.), ($))
 import "base" Data.Functor ((<$>))
 import "base" Data.String (String)
 import "base" System.IO (print)
 import "base" Text.Read (read)
 import "base" Text.Show (show)
-import "hedgehog" Hedgehog (Gen, Property, assert, forAll, property, (===))
-import "hedgehog" Hedgehog.Gen (string, enum)
+import "hedgehog" Hedgehog (Gen, Property, assert, forAll, property, success, (===))
+import "hedgehog" Hedgehog.Gen (enum, list, string)
 import "hedgehog" Hedgehog.Range (linear)
 
-import System.Monopati.Posix (Absolute, Relative, Path, To, Points (Directory, File), part, (</>), (<^>))
+import System.Monopati.Posix (Absolute, Relative, Path, To, Origin (Root, Vague), Points (Directory, File), part, (</^>), (<^^>))
 
 points :: Gen String
 points = string (linear 0 100) (enum 'a' 'z')
 
-relative :: Gen (Relative Path To 'File)
-relative = (<^>) <$> (part <$> points) <*> (part <$> points)
+generate_relative :: Gen (Relative Path To 'Directory)
+generate_relative = do
+	accumulator <- part <$> points
+	raw <- list (linear 1 10) (part @Vague @Directory <$> points)
+	pure $ foldr (<^^>) accumulator raw
 
-absolute :: Gen (Absolute Path To 'Directory)
-absolute = (</>) <$> (part <$> points) <*> (part <$> points)
+generate_absolute :: Gen (Absolute Path To 'Directory)
+generate_absolute = do
+	accumulator <- part <$> points
+	raw <- list (linear 1 10) (part @Vague @Directory <$> points)
+	pure $ foldr (flip (</^>)) accumulator raw
 
 concatenate_absolute_and_relative :: Property
-concatenate_absolute_and_relative = property $ do
-	(</>) <$> (forAll absolute) <*> (forAll relative)
-	assert True
+concatenate_absolute_and_relative = property $ success
 
 show_then_read_absolute :: Property
 show_then_read_absolute = property $ do
-	absolute' <- forAll absolute
-	(read . show) absolute' === absolute'
+	absolute <- forAll generate_absolute
+	(read . show) absolute === absolute
+
+show_then_read_relative :: Property
+show_then_read_relative = property $ do
+	relative <- forAll generate_relative
+	(read . show) relative === relative
